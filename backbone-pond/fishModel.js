b556da76fc8de95093c743b49f2acfafa446b3cb
afaqsmadi <afaqsmadi@gmail.com>
2017-09-09 09:42:23 +0300

    final solution

--- backbone-pond/fishModel.js
+++ backbone-pond/fishModel.js
@@ -8,5 +8,18 @@ var Fish = Backbone.Model.extend({
     description: 'Regular old fish',
     displayInfo: false
   }
+  toggleDescription:function(this.model.tr){
+  	// in this we 
+
+  }
 
 });
+// i need to make this.state that have the first value "defulat false " when i click its toggle 
+// this.state{
+// 	done:fale
+// }
+// the function is
+// onclick=()=>{
+
+// }
+// <tr onClick(){i call the function }
