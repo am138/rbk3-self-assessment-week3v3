8ab4eb9900dede3c48cd43f1f43b6647b7324050
DoaaAlsafadi <d.alsafadi.jo@gmail.com>
2017-09-09 09:29:36 +0300

    easyBackbone

--- backbone-pond/fishModel.js
+++ backbone-pond/fishModel.js
@@ -1,6 +1,10 @@
 // Please modify this file!
-
 var Fish = Backbone.Model.extend({
+
+	toggleDescription: () => (
+			this.set({displayInfo : !this.get("displayInfo")});
+		)
+
 
   defaults: {
     name: 'Larry',
