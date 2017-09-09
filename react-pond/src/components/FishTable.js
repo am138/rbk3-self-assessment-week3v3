9a5ec9476ca1c03f3f2fcdb4fc8a15e8bea0143e
Esam <esamsharaf91@gmail.com>
2017-09-09 09:37:19 +0200

    easyReact

--- react-pond/src/components/FishTable.js
+++ react-pond/src/components/FishTable.js
@@ -17,3 +17,24 @@ FishTable.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.FishTable = FishTable;
+
+
+// Refactoring -----> 
+
+var FishTable = (props) => (
+  <table>
+    <tbody>
+      <this.props.fishes.map(fish => {FishTableRow fish={fish}})
+    </tbody>
+  </table>
+);
+
+// PropTypes tell other developers what `props` a component expects
+// Warnings will be shown in the console when the defined rules are violated
+FishTable.propTypes = {
+  fishes: React.PropTypes.array.isRequired
+};
+
+// In the ES6 spec, files are "modules" and do not share a top-level scope
+// `var` declarations will only exist globally where explicitly defined
+window.FishTable = FishTable;
--- react-pond/src/components/FishTableRow.js
+++ react-pond/src/components/FishTableRow.js
@@ -29,3 +29,37 @@ FishTableRow.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.FishTableRow = FishTableRow;
+
+//Refactoring ---> 
+
+class FishTableRow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDescription: false
+    };
+  }
+
+  // Currently, the image being displayed is hardcoded from tinyurl.com 
+  render() {
+    return (
+      <tr onClick={() => this.setState({showDescription: !this.state.showDescription})}>
+        <td className="fish-name">{this.props.name}</td>
+        <td>
+          <img src={this.props.image} />
+        </td>
+        {this.state.showDescription ? <td className="fish-description">{this.props.description}</td> : null}
+      </tr>
+    )
+  }
+}
+
+// PropTypes tell other developers what `props` a component expects
+// Warnings will be shown in the console when the defined rules are violated
+FishTableRow.propTypes = {
+  fish: React.PropTypes.object.isRequired
+};
+
+// In the ES6 spec, files are "modules" and do not share a top-level scope
+// `var` declarations will only exist globally where explicitly defined
+window.FishTableRow = FishTableRow;
