6b171b39afa213b3e9c6de2e2ca3642f8b027f57
Roqaya Qazala <roukamohammad@gmail.com>
2017-09-09 09:40:26 +0300

    easyBackbone

--- backbone-pond/fishModel.js
+++ backbone-pond/fishModel.js
@@ -1,5 +1,16 @@
 // Please modify this file!
 
+/*var Fish = Backbone.Model.extend({
+
+  defaults: {
+    name: 'Larry',
+    image: 'http://www.google.com',
+    description: 'Regular old fish',
+    displayInfo: false
+  }
+
+});*/
+
 var Fish = Backbone.Model.extend({
 
   defaults: {
@@ -7,6 +18,14 @@ var Fish = Backbone.Model.extend({
     image: 'http://www.google.com',
     description: 'Regular old fish',
     displayInfo: false
+  },
+  //////we use toggle function to change the inFormation in displayInfo , if it true it will make it false and if it false it will make it true.
+  
+  	toggleDescription: function(){
+
+  		if(this.displayInfo!==this.displayInfo){
+  		return this.displayInfo;
+  	}
   }
 
 });
