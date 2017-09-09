6880ba002e75a83c90b5bc776ee2054c786ca615
rasha86 <rasha.abusamra86@gmail.com>
2017-09-09 08:48:52 +0300

    easyBackbone

--- backbone-pond/fishModel.js
+++ backbone-pond/fishModel.js
@@ -1,4 +1,6 @@
 // Please modify this file!
+// Inside of `fishModel.js`, create a `toggleDescription` function such that when a fish <tr> is clicked, the fish description will toggle on or off. DO NOT MODIFY any other files. 
+// * when you done, make a commit with the comment "easyBackbone"
 
 var Fish = Backbone.Model.extend({
 
