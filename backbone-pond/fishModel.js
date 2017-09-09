559f316c6125fbd4b6db5e1a8f8a5ba3f10020a8
Siraj Kakeh <sirajonline.89@gmail.com>
2017-09-09 09:40:51 +0300

    easyBackbone

--- backbone-pond/fishModel.js
+++ backbone-pond/fishModel.js
@@ -1,12 +1,14 @@
 // Please modify this file!
 
 var Fish = Backbone.Model.extend({
-
+  
   defaults: {
     name: 'Larry',
     image: 'http://www.google.com',
     description: 'Regular old fish',
     displayInfo: false
+  },
+  toggleDescription: function () {
+  	//since the watch is on description, we want to alter description with .set() method
   }
-
 });
